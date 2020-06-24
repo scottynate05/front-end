e594d6345d96fccc4fb32cb46c7105529b7207b0
dustin <dustnclaytiner@gmail.com>
2020-06-23 20:52:13 -0500

    adding props

--- dev-desk/src/components/StudentTicket.js
+++ dev-desk/src/components/StudentTicket.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import {Card, CardTitle} from 'reactstrap'
+
+
+
 const StudentTicket = (props) => {
     return(
+
+       
         <> 
         <Card>
             <CardTitle>
