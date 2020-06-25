4451e704a2907841bccbaf2e44c13e2565a963e8
dustin <dustnclaytiner@gmail.com>
2020-06-24 22:39:04 -0500

    ticketlists

--- dev-desk/src/components/TicketList.js
+++ dev-desk/src/components/TicketList.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Card,CardText} from 'reactstrap'
+import {Card,CardText,Button} from 'reactstrap'
 
 const TicketList = (props) => {
     return(
         <>
         <Card>
         <h4>{props.ticket.subject}</h4>
-        <h6>{props.ticket.messages[0].body}</h6>            
+        <h6>{props.ticket.messages[0].body}</h6>  
+        <Button color='primary'>Assign </Button>          
         </Card>
 
         </>
