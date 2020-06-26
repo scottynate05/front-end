f8f3079117d0d58507daa2389eddbc78fef1edf1
dustin <dustnclaytiner@gmail.com>
2020-06-25 20:44:23 -0500

    styling

--- dev-desk/src/components/TicketList.js
+++ dev-desk/src/components/TicketList.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {Card,CardText,Button} from 'reactstrap'
+import {Card,CardText,Button,Col} from 'reactstrap'
 
 const TicketList = (props) => {
     return(
         <>
-        <Card>
+        <Col sm='5'>
+        <Card style={{marginTop:'2%'}}>
         <h4>{props.ticket.subject}</h4>
-        <h6>{props.ticket.messages[0].body}</h6>  
-        <Button color='primary'>Assign </Button>          
+        <h6>{props.ticket.messages[0].body}</h6> 
+        <Col sm='3'>
+        <Button color='primary'>Assign </Button>   
+        </Col>
         </Card>
-
+        </Col>
         </>
     )
 }
