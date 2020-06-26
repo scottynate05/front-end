851a187ab0a3ad575547a0362dcbf354f1450b1f
dustin <dustnclaytiner@gmail.com>
2020-06-25 21:59:58 -0500

    props for staff ticket

--- dev-desk/src/components/StaffTicket.js
+++ dev-desk/src/components/StaffTicket.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {Card, CardSubtitle, Button,Row,Col} from 'reactstrap'
+import { useParams, useHistory, Link } from 'react-router-dom'
+import axios from 'axios'
+import axiosWithAuth from '../utils/axiosWithAuth'
 
-const StaffTicket =() => {
-    return(
-        <>
-        <h2>Staff Ticket</h2>
-        </>
-    )
+
+
+const StaffTicket = (props) => {
+
+const [staffTicket,setStaffTicket] =useState([])
+
+return(
+    <>
+    <Card style={{margin:'2%'}}>
+                    <h4>
+                        props
+                    </h4>
+                    <CardSubtitle>
+                        status: props
+                    </CardSubtitle>
+                    <h6>
+                        props
+                    </h6>
+                    <Col sm='3'>
+                    <Button color='primary'>Update Ticket</Button>
+                    </Col>
+                </Card>
+    </>
+)
 }
-
 export default StaffTicket
