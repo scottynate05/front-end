22a62fdf692fd2ce1a913b81ef5b9cc7dbb0da5f
Nathan Brown <scottynate05@yahoo.com>
2020-06-25 18:44:31 -0700

    updated open ticket list

--- dev-desk/src/components/TicketList.js
+++ dev-desk/src/components/TicketList.js
@@ -4,12 +4,16 @@ import {Card,CardText,Button} from 'reactstrap'
 const TicketList = (props) => {
     return(
         <>
-        <Card>
-        <h4>{props.ticket.subject}</h4>
-        <h6>{props.ticket.messages[0].body}</h6>  
-        <Button color='primary'>Assign </Button>          
-        </Card>
-
+        {
+            props.ticket.status === 'open' ?
+            (
+                <Card>
+                <h4>{props.ticket.subject}</h4>
+                <h6>{props.ticket.messages[0].body}</h6>  
+                <Button color='primary'>Help Student</Button>          
+                </Card>
+            ) : ''
+        }
         </>
     )
 }
